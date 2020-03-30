70e0242acc77f2dc3c102d42686ddc38ba307065
Junaid Chaudhary <junaid_chaudhary@external.mckinsey.com>
2020-03-30 23:44:56 +0530

    updated to latest cra

--- server.js
+++ server.js
@@ -1,44 +1,50 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const path = require('path');
-const app = express();
+const path = require('path')
+const app = express()
 const ObjectID = require('mongodb').ObjectID
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // DB config goes here
 const dbURI = require('./config/keys').dbURI
 // const dbRoute = "mongodb://localhost:27017/Building";  // Local DB
 
-mongoose.connect(
-  dbURI,
-  { useNewUrlParser: true })
-  .then(() => console.log('Database connected successfully'))
-  .catch((err) => console.log('Something went wrong connecting DB', err))
+mongoose
+	.connect(dbURI, { useNewUrlParser: true })
+	.then(() => console.log('Database connected successfully'))
+	.catch(err => console.log('Something went wrong connecting DB', err))
 
 const db = mongoose.connection
 
 app.get('/getPreferences', (req, res) => {
-  db.collection('preferences').find({}).toArray((err, data) => {
-    if (err) res.json({'success': false, 'error': err})
-    res.json({'success': true, data: data})
-  })
+	db.collection('preferences')
+		.find({})
+		.toArray((err, data) => {
+			if (err) res.json({ success: false, error: err })
+			res.json({ success: true, data: data })
+		})
 })
 
 app.post('/addPreferences', (req, res) => {
-  requestBody = req.body
-  requestBody.forEach(obj => {
-    let category = {}
-    category[obj.category] = obj.subCategories
-    console.log(category)
-    // let subCategories = obj.subCategories
-    db.collection('preferences').update({_id: obj.id},
-          {$set: category}, { multi: true }
-       )
-    }, () => res.send({'success': true}))
-});
+	requestBody = req.body
+	requestBody.forEach(
+		obj => {
+			let category = {}
+			category[obj.category] = obj.subCategories
+			console.log(category)
+			// let subCategories = obj.subCategories
+			db.collection('preferences').update(
+				{ _id: obj.id },
+				{ $set: category },
+				{ multi: true }
+			)
+		},
+		() => res.send({ success: true })
+	)
+})
 
 // app.get('/getData', (req, res) => {
 //   const name = (req.param)
@@ -49,8 +55,7 @@ app.post('/addPreferences', (req, res) => {
 //   })
 // })
 
-
-// Delete all the data from the DB 
+// Delete all the data from the DB
 // app.get('/deleteData', (req, res) => {
 //   db.collection('Building').deleteMany(
 //     {}, (err, data) => {
@@ -58,8 +63,6 @@ app.post('/addPreferences', (req, res) => {
 //           res.json({'result': 'Success', 'message': 'Data Deleted Successfully'})
 //         });
 // })
-
-
 
 // if (process.env.NODE_ENV === 'production') {
 //   // Serve any static files
@@ -70,5 +73,5 @@ app.post('/addPreferences', (req, res) => {
 //   });
 // }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
