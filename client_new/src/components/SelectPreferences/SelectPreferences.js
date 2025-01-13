c61936c66ea8e1452976a1f10ba8e70463ac937d
JustJunaid <junaidchoudhary210@gmail.com>
2025-01-14 00:11:02 +0530

    test

--- client_new/src/components/SelectPreferences/SelectPreferences.js
+++ client_new/src/components/SelectPreferences/SelectPreferences.js
@@ -1,20 +1,20 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function SelectPreferences() {
-// 	return (
-// 		<div className={classes.root}>
-// 			<GridList cellHeight={180} className={classes.gridListSubCategories}>
-// 				{subCategories[0].map((subCategory, i) => (
-// 					<GridListTile
-// 						key={i}
-// 						style={{ cursor: 'pointer' }}
-// 						value={subCategory}
-// 						onClick={selectSubCategory}
-// 					>
-// 						<GridListTileBar title={subCategory} />
-// 					</GridListTile>
-// 				))}
-// 			</GridList>
-// 		</div>
-// 	)
-// }
+export default function SelectPreferences() {
+	return (
+		<div className={classes.root}>
+			<GridList cellHeight={180} className={classes.gridListSubCategories}>
+				{subCategories[0].map((subCategory, i) => (
+					<GridListTile
+						key={i}
+						style={{ cursor: 'pointer' }}
+						value={subCategory}
+						onClick={selectSubCategory}
+					>
+						<GridListTileBar title={subCategory} />
+					</GridListTile>
+				))}
+			</GridList>
+		</div>
+	)
+}
